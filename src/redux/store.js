1ab75fd165dffaf0e01a928fbb959e16b2bd26ad
Alvan MAzid <alvantwent@gmail.com>
2023-11-13 19:50:07 +0700

    Vite_Mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,6 @@ import searchReducers from "./reducers/searchReducers";
 export default configureStore({
   reducer: rootReducers,
   search: searchReducers,
-  devTools: import.meta.VITE.MODE === "production",
+  devTools: import.meta.VITE_MODE !== "production",
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
